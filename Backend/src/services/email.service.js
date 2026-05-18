38137ae8d9a48111bfcdbd904b2053dccc946902
pranavUdiwal <pranavudiwal@gmail.com>
2026-05-18 16:02:53 +0530

    email verification updated

--- Backend/src/services/email.service.js
+++ Backend/src/services/email.service.js
@@ -1,12 +1,18 @@
 const nodemailer = require('nodemailer');
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    family: 4,
     auth: {
         user: process.env.EMAIL,
         pass: process.env.EMAIL_PASSWORD
+    },
+    tls: {
+        rejectUnauthorized: false
     }
-})
+});
 
 const sendMailTo = async (to, subject, html) => {
     const mailOptions = {
