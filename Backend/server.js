8756d952c98052f57dd2c5c7432b86ff5ca838f7
pranavUdiwal <pranavudiwal@gmail.com>
2026-05-18 16:10:33 +0530

    email dns updated

--- Backend/server.js
+++ Backend/server.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const dns = require('dns');
+dns.setDefaultResultOrder('ipv4first');
 const app = require('./src/app');
 const connectDB = require('./src/config/db');
 
--- Backend/src/services/email.service.js
+++ Backend/src/services/email.service.js
@@ -1,4 +1,7 @@
 const nodemailer = require('nodemailer');
+const dns = require('dns');
+
+dns.setDefaultResultOrder('ipv4first');
 
 const transporter = nodemailer.createTransport({
     host: 'smtp.gmail.com',
